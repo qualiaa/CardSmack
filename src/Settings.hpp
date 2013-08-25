73733ade339e598d5759b25b96c143154504bdc2
Gazok <jamiebayne@hotmail.com>
2013-08-25 04:22:55 +0100

    Added turnTime constant

--- src/Settings.hpp
+++ src/Settings.hpp
@@ -6,6 +6,7 @@ namespace settings
     constexpr unsigned int manaRegen = 20;
     constexpr unsigned int manaAmount = 8;
     constexpr unsigned int lifeAmount = 100;
+    constexpr unsigned int turnTime = 10;
 }
 
 #endif /* SETTINGS_HPP */
