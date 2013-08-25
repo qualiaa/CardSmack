772237bc6d465e2216d9c7737e9ef48eabae7763
Gazok <jamiebayne@hotmail.com>
2013-08-25 20:55:52 +0100

    Added random seed

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,11 @@
+#include <cstdlib>
+#include <ctime>
 #include <Tank/System/Game.hpp>
 #include "MainState.hpp"
 
 int main()
 {
+    std::srand(std::time(NULL));
     if(tank::Game::initialize({800,600}))
     {
         tank::Game::makeState<MainState>();
