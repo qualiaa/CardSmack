9e343d028cd69ef65808f11e0d9d0cbc145207f8
Gazok <jamiebayne@hotmail.com>
2013-08-26 05:51:27 +0100

    Added Resources file

---
+++ src/Resources.hpp
@@ -0,0 +1,17 @@
+#include <Tank/Graphics/Image.hpp>
+
+namespace res
+{
+    namespace abilities
+    {
+        tank::Image blockLeft("res/gfx/CardBlockL.png");
+        tank::Image blockRight("res/gfx/CardBlockR.png");
+        tank::Image directDamage("res/gfx/CardDirectDamate.png");
+        tank::Image doubleDamage("res/gfx/CardDoubleDamage.png");
+        tank::Image healthGain("res/gfx/CardHealthGain.png");
+        tank::Image manaGain("res/gfx/CardManaGain.png");
+    }
+
+    tank::Image powerIcon("res/gfx/CardPower.png");
+    tank::Image costIcon("res/gfx/CardCost.png");
+} /* namespace resources */
